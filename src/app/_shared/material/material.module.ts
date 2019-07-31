0a576784ebb10b0f17a14173dc0b7b4d9d0b7f07
Cristina Veale <cristinaveale@gmail.com>
2019-07-31 17:19:39 -0400

    updating from old site to new

---
+++ src/app/_shared/material/material.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from "@angular/core";
+import {
+  MatButtonModule,
+  MatSidenavModule,
+  MatListModule,
+  MatInputModule,
+  MatIconModule,
+  MatAutocompleteModule,
+  MatExpansionModule,
+  MatTooltipModule,
+  MatCardModule,
+  MatTabsModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatToolbarModule,
+  MatSelectModule,
+  MatMenuModule,
+  MatGridListModule
+} from "@angular/material";
+
+const modules: Array<any> = [
+  MatButtonModule,
+  MatSidenavModule,
+  MatListModule,
+  MatInputModule,
+  MatIconModule,
+  MatAutocompleteModule,
+  MatExpansionModule,
+  MatTooltipModule,
+  MatCardModule,
+  MatTabsModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatToolbarModule,
+  MatSelectModule,
+  MatMenuModule,
+  MatGridListModule
+];
+
+@NgModule({
+  imports: modules,
+  exports: modules
+})
+export class MaterialModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { SharedModule } from "./_shared/shared.module";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { SponsorsComponent } from "./sponsors/sponsors.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
+import { OrganizersComponent } from "./organizers/organizers.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    SponsorsComponent,
+    ContactComponent,
+    FooterComponent,
+    OrganizersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
