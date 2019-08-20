6e1f59605ec4935a2b0eb301f13cf50abee839e3
Cristina Veale <cristinaveale@gmail.com>
2019-08-20 15:28:01 -0400

    adding routing to TYS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { HomeComponent } from "./home/home.component";
+import { OrganizersComponent } from "./organizers/organizers.component";
+import { AboutComponent } from "./about/about.component";
+import { EventsComponent } from "./events/events.component";
+import { MembershipComponent } from "./membership/membership.component";
+import { SponsorsComponent } from "./sponsors/sponsors.component";
+import { ContactComponent } from "./contact/contact.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "about/organizations", component: OrganizersComponent },
+  { path: "about", component: AboutComponent },
+  { path: "events", component: EventsComponent },
+  { path: "membership", component: MembershipComponent },
+  { path: "sponsors", component: SponsorsComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "**", component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,29 +5,40 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { SharedModule } from "./_shared/shared.module";
 
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
 import { SponsorsComponent } from "./sponsors/sponsors.component";
 import { AboutComponent } from "./about/about.component";
 import { ContactComponent } from "./contact/contact.component";
 import { FooterComponent } from "./footer/footer.component";
 import { OrganizersComponent } from "./organizers/organizers.component";
+import { HomeComponent } from "./home/home.component";
+import { EventsComponent } from "./events/events.component";
+import { MembershipComponent } from "./membership/membership.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     AboutComponent,
     SponsorsComponent,
     ContactComponent,
     FooterComponent,
-    OrganizersComponent
+    OrganizersComponent,
+    HomeComponent,
+    EventsComponent,
+    MembershipComponent,
+    NotFoundComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     SharedModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
